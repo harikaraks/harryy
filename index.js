b26ff18a47bcd1dec010ebeb45b2b39fd8f4d2c5
harikaraks <86485936+harikaraks@users.noreply.github.com>
2021-06-28 02:44:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "./index.css";
+let curDate = new Date();
+curDate = curDate.getHours();
+let greeting ='';
+const cssStyle = { };
+if(curDate>=1 && curDate<12){
+  greeting = "GoodMorning";
+  cssStyle.color="green";
+}else if(curDate>=12 && curDate<19){
+  greeting="Good Afternoon";
+  cssStyle.color="orange";
+  ;
+}else{
+  greeting="Good Night";
+  cssStyle.color="yellow";
+}
+ 
+ReactDOM.render(
+  
+  <h1><center>Hello Sir, <span style={cssStyle}> { greeting }</span></center></h1>,
+  
+  document.getElementById("root")
+  
+);
